87f1a9f30483f0a7062bfd4c494f0518bc9b9803
awfulreternal <matveykudelin7@gmail.com>
2024-08-21 02:42:18 +0300

    Create generator.cpp

---
+++ compiler/generator.cpp
@@ -0,0 +1,7 @@
+#include "generator.h"
+
+// ... Генерация кода на целевом языке (JavaScript, C++, Python) ...
+
+void Generator::generate(AstNode* root) {
+  // ... Переход по AST, генерация кода на целевом языке ...
+}
