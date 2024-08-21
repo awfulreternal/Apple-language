9c95dd4b59aee2cc78c8d839d6a887431677ef33
awfulreternal <matveykudelin7@gmail.com>
2024-08-21 03:40:34 +0300

    Create main.cpp

---
+++ apple-interpreter/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include "lexer.hpp"
+#include "parser.hpp"
+#include "interpreter.hpp"
+
+int main() {
+  // 1. Считывание  исходного  кода  из  файла
+  // 2. Создание  Lexer
+  // 3. Создание  Parser
+  // 4. Создание  Interpreter
+  // 5. Выполнение  кода  с  помощью  Interpreter
+  
+  return 0;
+}
